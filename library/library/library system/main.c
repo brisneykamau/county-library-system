4f946224a660d70d5f6ee2dd461e90f6f621810d
brisneykamau <99818274+brisneykamau@users.noreply.github.com>
2022-03-02 22:01:42 +0300

    Update main.c

--- library/library/library system/main.c
+++ library/library/library system/main.c
@@ -1,6 +1,6 @@
 /*
   project: County library management system.
-  Author: Delan Mugure
+  Author: Brisney Kamau
   Date: 29,Feb
   compiler: GNU GCC
   Language: C99'
@@ -12,7 +12,7 @@
  int main ()
  {
      printf("\tCounty Library\n");
-     printf("Welcome Mr. Delan.\n");
+     printf("Welcome Ms.Brisney\n");
      printf("What would you like to do?\n");
      printf("1.Add new User\n");
      printf("2.Add book.\n");
